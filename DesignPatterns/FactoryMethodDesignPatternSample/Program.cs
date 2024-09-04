7f98b9ee51d3a8a7d276ed86dd92d73e03373ab5
Parsa Fathi <dayihidden@gmail.com>
2024-09-04 22:33:07 +0430

    Added a new project Called "Factory Method".

---
+++ DesignPatterns/FactoryMethodDesignPatternSample/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FactoryMethodDesignPatternSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("this will be factory method as soon as possible");
+        }
+    }
+}
