e43355255078b9db08c886cd4584b15989baaaf7
Parsa Fathi <dayihidden@gmail.com>
2024-09-04 22:17:41 +0430

    i implemented an example of Singleton design pattern.

---
+++ DesignPatterns/SingletonDesignPatternSample/Counter.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPatternSample
+{
+    public class Counter
+    {
+
+        
+        
+            private static Counter _instance;
+
+            private static readonly object _lock = new object();
+
+            private int _count;
+                                                              
+            private Counter()
+            {
+                _count = 0;
+            }
+
+            public static Counter Instance
+            {
+
+                get
+                {
+                    lock (_lock)
+                    {
+                        if (_instance == null)
+                        {
+                            _instance = new Counter();
+                        }
+                    }
+                    return _instance;
+                }
+            }
+
+            public void Increment() { _count++; }
+
+            public int GetCount()
+            {
+
+                return _count;
+
+            }
+        }
+
+    }
+
---
+++ DesignPatterns/SingletonDesignPatternSample/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SingletonDesignPatternSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter1 = Counter.Instance;
+            counter1.Increment();
+            counter1.Increment();
+            Console.WriteLine("Counter1: " + counter1.GetCount());
+
+
+            Counter counter2 = Counter.Instance;
+            counter2.Increment();
+            Console.WriteLine("Counter2: " + counter2.GetCount());
+
+
+            Console.WriteLine(Object.ReferenceEquals(counter1, counter2));
+        }
+    }
+}
